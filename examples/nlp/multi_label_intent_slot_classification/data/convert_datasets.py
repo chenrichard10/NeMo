db1ad4a48627f6a63cb685e0d11ba5cedd9242bc
Richard Chen <chenpipi@live.com>
2022-01-18 10:49:04 -0500

    Multi-Label Joint Intent Slot Classification
    Multi-Label Joint Intent Classification
    
    Multi-Label Training
    
    Multi-label training functionality
    
    classification report
    
    Classification report
    
    typo
    
    Multi-Label model functionality
    
    Loss function renaming
    
    Optimize Threshold
    
    Tutorial Notebook Nemo Format Script
    
    Notebook and Dataset updates
    
    File conversion bug fix
    
    File conversion fix
    
    file reformatting
    
    augmentation data script changes
    
    augmentation data script changes
    
    augmentation data script changes
    
    augmentation data script changes
    
    Jenkinsfile
    
    Top Three Intents
    
    Jenkins test fix

---
+++ examples/nlp/multi_label_intent_slot_classification/data/convert_datasets.py
@@ -0,0 +1,102 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+import os
+import shutil
+from os.path import exists
+
+import pandas as pd
+
+
+def convert_atis_multi_label(path, source_dir, target_dir, mode):
+    """
+    Converts single label atis nemo data to multi-label data. Previous 
+    labels in Nemo mapped multi-labels to a single index rather than two separate indicies.
+
+    path: path to csv file to convet
+    source_dir: directory that stored original nemo files
+    target_dir: directory to store multi-label nemo files
+    """
+    data = pd.read_csv(path, sep='\t')
+    # Get the original intent dictionary
+    intents = pd.read_csv(f'{source_dir}/dict.intents.csv', sep='\t', header=None)
+    # Create a new intent dictionary
+    old_intents = []
+    #  Get the original intents
+    for index, intent in intents.iterrows():
+        old_intents.append(intent[0])
+
+    # Create new intent list mapping
+    new_intents = [x for x in old_intents if "+" not in x]
+
+    # New dictionary to convert to dataframe
+    intent_labels = []
+
+    for index, intent in data.iterrows():
+        temp_dict = {}
+        temp_dict['sentence'] = intent['sentence']
+        old_label = old_intents[int(intent['label'])]
+
+        values = [old_label]
+
+        if '+' in old_label:
+            values = old_label.split('+')
+
+        for index, label in enumerate(new_intents):
+            if label in values:
+                if 'label' not in temp_dict:
+                    temp_dict['label'] = f"{index}"
+                else:
+                    temp_dict['label'] = f"{temp_dict['label']},{index}"
+
+        intent_labels.append(temp_dict)
+
+    multi_intent_df = pd.DataFrame.from_dict(intent_labels)
+    multi_intent_df.to_csv(f'{target_dir}/{mode}.tsv', sep='\t', index=False)
+
+
+def convert_intent_dictionary(source_dir, target_dir):
+    intents = pd.read_csv(f'{source_dir}/dict.intents.csv', sep='\t', header=None)
+
+    new_atis_dict_list = []
+
+    for index, intent in intents.iterrows():
+        new_atis_dict_list.append(intent[0])
+
+    new_intents = [x for x in new_atis_dict_list if "+" not in x]
+    df = pd.DataFrame(new_intents)
+    df.to_csv(f"{target_dir}/dict.intents.csv", index=False, header=False)
+
+
+if __name__ == "__main__":
+    # Parse the command-line arguments.
+    parser = argparse.ArgumentParser(description="Process and convert datasets into NeMo\'s format.")
+    parser.add_argument(
+        "--source_data_dir", required=True, type=str, help='path to the folder containing the dataset files'
+    )
+    parser.add_argument("--target_data_dir", required=True, type=str, help='path to save the processed dataset')
+
+    args = parser.parse_args()
+
+    source_dir = args.source_data_dir
+    target_dir = args.target_data_dir
+
+    convert_intent_dictionary(f'{source_dir}', f'{target_dir}')
+    convert_atis_multi_label(f'{source_dir}/train.tsv', f'{source_dir}', f'{target_dir}', 'train')
+    convert_atis_multi_label(f'{source_dir}/test.tsv', f'{source_dir}', f'{target_dir}', 'dev')
+    shutil.copyfile(f'{source_dir}/dict.slots.csv', f'{target_dir}/dict.slots.csv')
+    shutil.copyfile(f'{source_dir}/train_slots.tsv', f'{target_dir}/train_slots.tsv')
+    shutil.copyfile(f'{source_dir}/test_slots.tsv', f'{target_dir}/dev_slots.tsv')
